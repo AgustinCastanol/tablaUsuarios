858ee55a74e96c5f8a7c7c34ad2280041b756290
AgustinCastanol <acastanol@fi.uba.ar>
2022-10-18 16:20:55 -0300

    correccion de funcionalidad

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,8 @@
   </div>
   <div class="col-start-2 row-start-1 col-end-4 inline-grid gap-2 grid-cols-2 ">
     <label class="" for="cuotaMax cuotaMin">Cuota:</label>
-    <!-- <input class="col-start-2" step=0.01 placeholder="min" type="number"  id="cantidadAfiliados" v-model="cuota['cuotaMax']">
-    <input class="col-start-2" step=0.01 placeholder="max" type="number" id="cantidadAfiliados" v-model="cuota['cuotaMin']"> -->
-      <p class=" min-w-full w-max col-start-2 text-left bg-slate-700">Maximo:{{cuota['cuotaMax']}}</p>
-      <p class=" min-w-full w-max col-start-2 text-left bg-slate-700">Minimo:{{cuota['cuotaMin']}}</p>
+    <input class="col-start-2" step=0.01 placeholder="min" type="number"  id="cantidadAfiliados" v-model="form['cuotaMax']">
+    <input class="col-start-2" step=0.01 placeholder="max" type="number" id="cantidadAfiliados" v-model="form['cuotaMin']">
   </div>
   <div class="flex items-start flex-col col-start-1 row-start-2">
     <label class="" for="importeTotal">Importe Total:</label>
@@ -18,44 +16,51 @@
   <button class=" col-start-2 row-start-3" @click="loadTable($event)">Aceptar</button>
 </form>
 
-  <table v-if="tabla.length != 0 " class="table-auto rounded w-full">
+  <table v-if="tabla.length != 0 " class="table table-auto w-full">
     <thead>
       <tr>
-        <th>Cantidad De Afiliados</th>
-        <th>Cuota Maxima</th>
-        <th>Cuota Minima</th>
-        <th>Importe Total</th>
+        <th>Afiliado</th>
+        <th>Cuota</th>
       </tr>
     </thead>
     <tbody>
       <tr v-for="item in tabla">
-        <td>{{item.cantidadAfiliados}}</td>
-        <td>{{item.cuotaMax}}</td>
-        <td>{{item.cuotaMin}}</td>
-        <td>{{item.importeTotal}}</td>
+        <td>{{item.afiliado}}°</td>
+        <td>${{item.cuota}}</td>
       </tr>
     </tbody>
+    <tfoot>
+      <tr >
+        <td>Total</td>
+        <td>${{totalSum}}</td>
+      </tr>
+    </tfoot>
   </table>
 </template>
 <script setup>
 import { ref } from 'vue'
-
-const cuota = ref({
-  cuotaMax: null,
-  cuotaMin: null,
-
-})
+const totalSum = ref(0)
 const form = ref({
   cantidadAfiliados:null,
-  importeTotal:null
+  importeTotal:null,
+  cuotaMax: null,
+  cuotaMin: null,
 })
 const tabla = ref([])
 const loadTable = (e) => {
   e.preventDefault()
   console.log(form.value)
 
-  for(let key in form.value[0]){
-    if(form.value[key] == null || form.value[key] == undefined || form.value[key] == 0){
+  if(form .value['cuotaMax'] == null || form.value['cuotaMax'] == 0){
+    console.log("grandote")
+    form.value['cuotaMax'] = form.value['importeTotal']
+  }
+  if(form.value['cuotaMin'] == null || form.value['cuotaMin'] == 0){
+    form.value['cuotaMin'] = 0
+    console.log("chiquito")
+  }
+  for(let key in form.value){
+    if(form.value[key] == null || form.value[key] == undefined ){
       alert('Complete todos los campos')
       return
     }
@@ -63,32 +68,72 @@ const loadTable = (e) => {
       alert('No se admiten valores negativos')
       return
     }
+    if(key == 'cantidadAfiliados' || key == 'importeTotal'){
+      if(form.value[key] % 1 != 0){
+        alert('No se admiten valores decimales')
+        return
+      }
+      if(form.value[key] == 0){
+        alert('No se admiten valores nulos')
+        return
+      }
+    }
     if(form.value[key] > 1000000000){
       alert('No se admiten valores tan grandes')
       return
     }
   }
-  handleRandomsMinMax()
-  // if(cuota.value['cuotaMin'] > cuota.value['cuotaMax']){
-  //   alert('La cuota minima no puede ser mayor a la cuota maxima')
-  //   return
-  // }
-  console.log(cuota.value)
-  form.value.cuotaMax = cuota.value['cuotaMax']
-  form.value.cuotaMin = cuota.value['cuotaMin']
-  tabla.value.push(form.value)
-  form.value = {
-    cantidadAfiliados:null,
-    cuotaMax:null,
-    cuotaMin:null,
-    importeTotal:null
+  if(form.value['cuotaMax'] < form.value['cuotaMin']){
+    alert('La cuota maxima no puede ser menor a la cuota minima')
+    return
   }
+  
+  handleRandomsMinMax()
+
+  // form.value = {
+  //   cantidadAfiliados:null,
+  //   cuotaMax:null,
+  //   cuotaMin:null,
+  //   importeTotal:null
+  // }
 }
 const handleRandomsMinMax = () => {
-  let min = form.value.importeTotal / form.value.cantidadAfiliados
-  let max = min * 1.5
-  cuota.value['cuotaMax'] = Math.round(max * 100) / 100
-  cuota.value['cuotaMin'] = Math.round(min * 100) / 100
+  /*Create a random number within a range, without the sum of the generated values ​​exceeding the total */
+  let total = form.value['importeTotal']
+  let sum = 0
+  let afiliado = 0
+  let max = form.value['cuotaMax']
+  let min = form.value['cuotaMin']
+  tabla.value = []
+  while(afiliado < form.value['cantidadAfiliados']){
+    let random = Math.random() * (max - min + 1) + min
+    random = Math.round(random * 100) / 100
+    if(sum + random <= total){
+      sum += random
+      if(random == 0){
+        tabla.value[afiliado-1].cuota =tabla.value[afiliado-1].cuota/2
+        sum -=tabla.value[afiliado-1].cuota
+        tabla.value.push({
+          afiliado: afiliado+1,
+          cuota: tabla.value[afiliado-1].cuota
+        })
+      }else{
+        tabla.value.push({
+          afiliado: afiliado+1,
+          cuota: random
+        })
+      }
+      afiliado++
+    }
+  }
+  if(sum < total){
+    tabla.value[tabla.value.length-1].cuota =tabla.value[tabla.value.length-1].cuota + Math.round((total - sum) * 100) / 100
+    sum =total
+  }
+  totalSum.value = sum
+
+
 }
+
 </script>
 
